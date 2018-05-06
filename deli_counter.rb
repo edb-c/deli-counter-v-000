d691d92c0e0adc6fadee605fbe815773d9c694e3
Ethel C <cofresi.ethel@gmail.com>
2018-05-06 01:13:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,37 +1,25 @@
-#katz_deli = ["First_Customer", "Second_Customer","Third_Customer"]
-#katz_deli = []
+katz_deli = []
 
-def line(input_array)
-
-  output_array = []
-
-  if input_array.length == 0
+def line(x)
+  line_array = []
+  if x.length == 0
     puts "The line is currently empty."
   else
-    input_array.each.with_index(1) do |name, index|
-      output_array.push("#{index}. #{name}")
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
     end
-    puts "The line is currently: #{output_array.join(" ")}"
+    puts "The line is currently: #{line_array.join(" ")}"
   end
-
 end
- 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length-1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-
-
-def now_serving(katz_deli)
-  if katz_deli.empty?
+def now_serving(array)
+  if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+    puts "Currently serving #{array[0]}."
+    array.shift
   end
 end
- 
-
-#line(katz_deli) 
-#take_a_number(katz_deli,"Third_Customer")
-#now_serving(katz_deli)
