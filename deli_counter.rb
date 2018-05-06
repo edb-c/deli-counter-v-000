75812b60d5d35154111ff210020769a6bba943c8
Ethel C <cofresi.ethel@gmail.com>
2018-05-06 00:36:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,25 @@
+katz_deli = []
 
-def take_a_number(katzDeliLine, name) 
-   katzDeliLine.push(name);
-   return "Welcome, #{name}. You are number #{katzDeliLine.length} in line.";
-end
-
-def now_serving(katzDeliLine)
-  if katzDeliLine.empty?
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katzDeliLine[0]}."
-    katzDeliLine.shift
-  end
-end
-
-def line(katzDeliLine)
-  line_array= []
-  if katzDeliLine.length == 0
+def line(x)
+  line_array = []
+  if x.length == 0
     puts "The line is currently empty."
   else
-    katzDeliLine.each.with_index(1) do |name, index|
+    x.each.with_index(1) do |name, index|
       line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
   end
 end
